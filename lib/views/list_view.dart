4b572aa671d1993ccaea450f417f1984795e9527
Mauricio Arias R <marias@lsv-tech.com>
2022-06-15 16:04:43 -0500

    agregando lottie

--- lib/views/list_view.dart
+++ lib/views/list_view.dart
@@ -904,7 +904,7 @@ class listview extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Lugares Exoticos '),
+          title: const Text('Shakira y yo '),
         ),
         body: ListView.builder(
           itemCount: lugares.length, //tamaño de la lsita
